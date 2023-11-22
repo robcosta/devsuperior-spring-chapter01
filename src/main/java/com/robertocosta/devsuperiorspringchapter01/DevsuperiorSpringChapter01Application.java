3a003485f0eff71b6122c90a5d5f7511ece54a2b
Roberto Costa <robertocosta66@gmail.com>
2023-11-22 13:27:50 -0300

    Created project

---
+++ src/main/java/com/robertocosta/devsuperiorspringchapter01/DevsuperiorSpringChapter01Application.java
@@ -0,0 +1,13 @@
+package com.robertocosta.devsuperiorspringchapter01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorSpringChapter01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperiorSpringChapter01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertocosta/devsuperiorspringchapter01/DevsuperiorSpringChapter01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertocosta.devsuperiorspringchapter01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorSpringChapter01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
